1e69c93c3e3f786c4002423a39c23f5a5cdf7ff6
Macchiato <simppp@gmail.com>
2024-07-02 18:34:36 +0900

    GUI 개선

--- Assets/Editor/SuyasuyaFacialEditor.cs
+++ Assets/Editor/SuyasuyaFacialEditor.cs
@@ -22,6 +22,8 @@ namespace com.vrsuya.suyasuyafacial {
 
 		public static int LanguageIndex = 0;
         public readonly string[] LanguageType = new[] { "English", "한국어", "日本語" };
+		public static bool FoldAvatar = false;
+		public static bool FoldAnimation = false;
 
 		void OnEnable() {
             SerializedAvatarGameObject = serializedObject.FindProperty("AvatarGameObject");
@@ -42,41 +44,47 @@ namespace com.vrsuya.suyasuyafacial {
 			EditorGUILayout.PropertyField(SerializedAvatarHeadSkinnedMeshRenderer, new GUIContent(LanguageHelper.GetContextString("String_TargetMesh")));
 			EditorGUILayout.PropertyField(SerializedAvatarFXAnimatorController, new GUIContent(LanguageHelper.GetContextString("String_TargetFXLayer")));
 			EditorGUILayout.PropertyField(SerializedTargetAnimationClips, new GUIContent(LanguageHelper.GetContextString("String_TargetAnimations")));
-			EditorGUI.indentLevel++;
-			if (SerializedTargetBlendShapes.arraySize > 0) {
-				EditorGUILayout.LabelField(LanguageHelper.GetContextString("String_TargetBlendShape"), EditorStyles.boldLabel);
-				for (int Index = 0; Index < SerializedTargetBlendShapes.arraySize; Index++) {
-					SerializedProperty BlendShapeProperty = SerializedTargetBlendShapes.GetArrayElementAtIndex(Index);
-					SerializedProperty ActiveValueProperty = BlendShapeProperty.FindPropertyRelative("ActiveValue");
-					SerializedProperty BlendShapeNameProperty = BlendShapeProperty.FindPropertyRelative("BlendShapeName");
-					EditorGUILayout.BeginHorizontal();
-					ActiveValueProperty.boolValue = EditorGUILayout.ToggleLeft(BlendShapeNameProperty.stringValue, ActiveValueProperty.boolValue);
-					EditorGUILayout.EndHorizontal();
-				}
-			} else {
-				EditorGUILayout.LabelField(LanguageHelper.GetContextString("String_TargetBlendShape"), EditorStyles.boldLabel);
-				EditorGUILayout.HelpBox(LanguageHelper.GetContextString("NO_SHAPEKEY"), MessageType.Info);
-			}
-			if (SerializedTargetAnimationBlendShapes.arraySize > 0) {
-				EditorGUILayout.LabelField(LanguageHelper.GetContextString("String_TargetAnimationBlendShape"), EditorStyles.boldLabel);
-				for (int Index = 0; Index < SerializedTargetAnimationBlendShapes.arraySize; Index++) {
-					SerializedProperty BlendShapeProperty = SerializedTargetAnimationBlendShapes.GetArrayElementAtIndex(Index);
-					SerializedProperty ActiveValueProperty = BlendShapeProperty.FindPropertyRelative("ActiveValue");
-					SerializedProperty BlendShapeNameProperty = BlendShapeProperty.FindPropertyRelative("BlendShapeName");
-					EditorGUILayout.BeginHorizontal();
-					ActiveValueProperty.boolValue = EditorGUILayout.ToggleLeft(BlendShapeNameProperty.stringValue, ActiveValueProperty.boolValue);
-					EditorGUILayout.EndHorizontal();
-				}
-			} else {
-				EditorGUILayout.LabelField(LanguageHelper.GetContextString("String_TargetAnimationBlendShape"), EditorStyles.boldLabel);
-				EditorGUILayout.HelpBox(LanguageHelper.GetContextString("NO_ANIMSHAPEKEY"), MessageType.Info);
-			}
-			EditorGUI.indentLevel--;
 			if (GUILayout.Button(LanguageHelper.GetContextString("String_Reload"))) {
 				(target as SuyasuyaFacial).ReloadVariable();
 				Repaint();
 			}
+			EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+			EditorGUI.indentLevel++;
+			if (SerializedTargetBlendShapes.arraySize > 0) {
+				FoldAvatar = EditorGUILayout.Foldout(FoldAvatar, LanguageHelper.GetContextString("String_TargetBlendShape"));
+				if (FoldAvatar) {
+					for (int Index = 0; Index < SerializedTargetBlendShapes.arraySize; Index++) {
+						SerializedProperty BlendShapeProperty = SerializedTargetBlendShapes.GetArrayElementAtIndex(Index);
+						SerializedProperty ActiveValueProperty = BlendShapeProperty.FindPropertyRelative("ActiveValue");
+						SerializedProperty BlendShapeNameProperty = BlendShapeProperty.FindPropertyRelative("BlendShapeName");
+						EditorGUILayout.BeginHorizontal();
+						ActiveValueProperty.boolValue = EditorGUILayout.ToggleLeft(BlendShapeNameProperty.stringValue, ActiveValueProperty.boolValue);
+						EditorGUILayout.EndHorizontal();
+					}
+				}
+			} else {
+				EditorGUILayout.LabelField(LanguageHelper.GetContextString("String_TargetBlendShape"));
+				EditorGUILayout.HelpBox(LanguageHelper.GetContextString("NO_SHAPEKEY"), MessageType.Info);
+			}
 			EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+			if (SerializedTargetAnimationBlendShapes.arraySize > 0) {
+				FoldAnimation = EditorGUILayout.Foldout(FoldAnimation, LanguageHelper.GetContextString("String_TargetAnimationBlendShape"));
+				if (FoldAnimation) {
+					for (int Index = 0; Index < SerializedTargetAnimationBlendShapes.arraySize; Index++) {
+						SerializedProperty BlendShapeProperty = SerializedTargetAnimationBlendShapes.GetArrayElementAtIndex(Index);
+						SerializedProperty ActiveValueProperty = BlendShapeProperty.FindPropertyRelative("ActiveValue");
+						SerializedProperty BlendShapeNameProperty = BlendShapeProperty.FindPropertyRelative("BlendShapeName");
+						EditorGUILayout.BeginHorizontal();
+						ActiveValueProperty.boolValue = EditorGUILayout.ToggleLeft(BlendShapeNameProperty.stringValue, ActiveValueProperty.boolValue);
+						EditorGUILayout.EndHorizontal();
+					}
+				}
+			} else {
+				EditorGUILayout.LabelField(LanguageHelper.GetContextString("String_TargetAnimationBlendShape"));
+				EditorGUILayout.HelpBox(LanguageHelper.GetContextString("NO_ANIMSHAPEKEY"), MessageType.Info);
+			}
+			EditorGUI.indentLevel--;
+			EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 			if (!string.IsNullOrEmpty(SerializedStatusCode.stringValue)) {
 				EditorGUILayout.HelpBox(ReturnStatusString(SerializedStatusCode.stringValue), MessageType.Warning);
             }
